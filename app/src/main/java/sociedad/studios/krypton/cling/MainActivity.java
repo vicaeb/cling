cf80193858b110c2aa9a9cca627089ac011fab9e
victor <vicaeb_onix@hotmail.com>
2017-05-19 18:23:30 -0500

    Subiendo cambios.

--- app/src/main/java/sociedad/studios/krypton/cling/MainActivity.java
+++ app/src/main/java/sociedad/studios/krypton/cling/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Comprobando que funcione nuestro control de versiones.", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
